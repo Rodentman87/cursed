6b96b51110db7ce849d93e7fd4c7a69745722ee1
Maisy <rodentman87@gmail.com>
2022-06-18 01:42:40 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import { Intents } from 'discord.js';
+import path from 'path';
+import { SlashasaurusClient } from 'slashasaurus';
+
+const client = new SlashasaurusClient(
+	{
+		intents: [Intents.FLAGS.GUILDS],
+	},
+	{
+		devServerId: '561807594516381749',
+	}
+);
+
+client.once('ready', () => {
+	console.log(`Client ready and logged in as ${client.user?.tag}`);
+	client.registerCommandsFrom(
+		path.join(__dirname, 'commands'),
+		true,
+		process.env.TOKEN!
+	);
+});
+
+client.login(process.env.TOKEN);
